cd11d3aa888ffbb97a66945c3cf67bcbfafebded
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2022-03-05 20:23:58 -0500

    Converted initparms, and initialize to go calls

--- vrapi.go
+++ vrapi.go
@@ -1,9 +1,56 @@
+//go:build darwin || linux || windows
+// +build android, gldebug
 package vrapi
 
+/*
+#cgo CPPFLAGS: -I./Include -I/usr/local/include
+#cgo LDFLAGS: -v -march=armv8-a -shared -L./lib/arm64-v8a/ -lvrapi -landroid
+
+#include <VrApi.h>
+#include <VrApi_Helpers.h>
+*/
 import "C"
 
-type OVRInitParams C.ovrInitParms // HMMM alias this type?
+import (
+	"fmt"
+	"unsafe"
+)
 
-func DefaultInitParms() OVRInitParms {
-	return C.vrapi_DefaultInitParms()
+const (
+	INITIALIZE_SUCCESS = C.VRAPI_INITIALIZE_SUCCESS
+)
+
+type OVRInitParms C.ovrInitParms // HMMM alias this type?
+type OVRJava C.ovrJava
+
+func DefaultInitParms(java *OVRJava) OVRInitParms {
+	cParms := C.vrapi_DefaultInitParms((*C.ovrJava)(java))
+	return OVRInitParms(cParms)
+}
+
+// This should run with vrctx like glctx on a seperate thread.
+// Could be intresting / helpful to seperate functions on seperate threads.
+// Following same worker pattern as gl.
+// Should this be a status? or an error code
+func Initialize(parms *OVRInitParms) error {
+	status := C.vrapi_Initialize((*C.ovrInitParms)(parms))
+	if status != INITIALIZE_SUCCESS {
+		return fmt.Errorf("vrapi_Initialize status %d not equal to sucess %d",
+			status, INITIALIZE_SUCCESS)
+	}
+	return nil
+}
+
+// Helpers not in the original API.
+// Expects the values from
+// app.RunOnJVM from the "golang.org/x/mobile/app" package
+// This function is behind an android build contraint.
+// https://github.com/golang/mobile/blob/8a0a1e50732f652b0c7a0ef4a9f6903c5dc0ca13/app/android.go#L73
+func CreateJavaObject(vm, jniEnv, ctx uintptr) OVRJava {
+	var java OVRJava
+	java.Vm = (*C.JavaVM)(unsafe.Pointer(vm))
+	java.Env = (*C.JNIEnv)(unsafe.Pointer(jniEnv))
+	java.ActivityObject = (C.jobject)(unsafe.Pointer(ctx))
+
+	return java
 }
